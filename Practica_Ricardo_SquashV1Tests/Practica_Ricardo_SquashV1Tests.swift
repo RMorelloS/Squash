b2820de6205102c63bcddcf412f1767b6333d6da
Ricardo Morello Santos <ricardo_morello@hotmail.com>
2019-10-08 17:34:50 -0500

    Initial Commit

---
+++ Practica_Ricardo_SquashV1Tests/Practica_Ricardo_SquashV1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Practica_Ricardo_SquashV1Tests.swift
+//  Practica_Ricardo_SquashV1Tests
+//
+//  Created by Morello Santos Ricardo on 10/8/19.
+//  Copyright © 2019 Morello Santos Ricardo. All rights reserved.
+//
+
+import XCTest
+@testable import Practica_Ricardo_SquashV1
+
+class Practica_Ricardo_SquashV1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
